0e5e6b57413a113638f877b021b1ab19f91e4e9d
iTeo <barmin.andrey.nnov@gmail.com>
2023-06-05 20:36:18 +0300

    Добавьте файлы проекта.

---
+++ RabbitMQ.Receiver/Program.cs
@@ -0,0 +1,58 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Start");
+
+ConnectionFactory factory = new ConnectionFactory();
+//factory.Uri = new(uriString: $"amqp://user:password@localhost:5672");
+factory.Uri = new(uriString: $"amqp://localhost");
+factory.Port = 5672;
+factory.UserName = "user";
+factory.Password = "password";
+factory.ClientProvidedName = "RabbitMQ Receiver";
+
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+string exchangeName = "rabbit-exchange";
+string routingKey = "rabbit-routing-key";
+string queueName = "RabbitQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName,
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: default);
+
+channel.QueueBind(queueName,
+    exchangeName,
+    routingKey,
+    arguments: default);
+
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, args) =>
+{
+    Task.Delay(1000).Wait();
+    var Body = args.Body.ToArray();
+    string message = Encoding.UTF8.GetString(Body);
+    Console.WriteLine($"[{DateTime.Now.ToString()}]  Received: {message}");
+    channel.BasicAck(args.DeliveryTag, multiple: false);
+};
+
+string consumerTag = channel.BasicConsume(queueName, autoAck: false, consumer: consumer);
+
+Console.WriteLine("press any key to continue");
+Console.ReadLine();
+
+channel.BasicCancel(consumerTag);
+
+channel.Close();
+connection.Close();
+
+Console.WriteLine("End");
---
+++ RabbitMQ.Sender/Program.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Start", ConsoleColor.DarkGreen);
+
+
+ConnectionFactory factory = new ConnectionFactory();
+//factory.Uri = new(uriString: $"amqp://user:password@localhost:5672");
+factory.Uri = new(uriString: $"amqp://localhost");
+factory.Port = 5672;
+factory.UserName = "user";
+factory.Password = "password";
+factory.ClientProvidedName = "RabbitMQ Sender";
+
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+string exchangeName = "rabbit-exchange";
+string routingKey = "rabbit-routing-key";
+string queueName = "RabbitQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, 
+    durable: false, 
+    exclusive: false, 
+    autoDelete: false, 
+    arguments: default);
+
+channel.QueueBind(queueName, 
+    exchangeName, 
+    routingKey, 
+    arguments: default);
+
+for(int i = 0; i < 15; i++)
+{
+    Task.Delay(555).Wait();
+
+    string message = $"Hello World! v{i}";
+    var messageBytes = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchangeName, routingKey, basicProperties: default, messageBytes);
+
+    Console.WriteLine($"[{DateTime.Now.ToString()}]  Sended {message}");
+}
+
+channel.Close();
+connection.Close();
+
+Console.WriteLine("Done", ConsoleColor.Magenta);
